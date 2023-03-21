c571dedca3ae1c5ea44fa6f2e5d4816cad0964d8
Frederik0111 <fredez0111@gmail.com>
2023-03-21 22:46:18 +0100

    frodo

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// loading javascript
+window.addEventListener("load", initApp);
+
+function initApp() {
+    const frodoName = "Frodo Baggings";
+    const frodoGender = "male";
+    const frodoBirthPlace = "The Shire";
+    const frodoAge = "50";
+    const frodoSpecies = "hobbit";
+    const frodoEyeColour = "blue";
+    const frodoHairColour = "brown";
+    const frodoActor = "Elijah Wood";
+    const frodoImage = "./images/Frodo.png";
+}
+
