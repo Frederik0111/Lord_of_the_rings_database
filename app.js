5aed33c31f8dec9c1dd778d9d8b17d965dbae711
Frederik0111 <fredez0111@gmail.com>
2023-03-27 18:59:26 +0200

    json work V0.1

--- app.js
+++ app.js
@@ -2,16 +2,3 @@
 
 // loading javascript
 window.addEventListener("load", initApp);
-
-function initApp() {
-    const aragonName = "Aragon";
-    const aragonGender = "male";
-    const aragonBirthPlace = "Eriador";
-    const aragonAge = "88";
-    const aragonSpecies = "human";
-    const aragonEyeColour = "blue";
-    const aragonHairColour = "dark brown";
-    const aragonActor = "Viggo Mortensen";
-    const aragonImage = "./images/aragon.png";
-}
-
